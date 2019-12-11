d4667d64bed0356388c94f7dbca0db98b884ad0b
1629477 <1629477@GY355X20>
2019-12-11 15:07:58 +0000

    Initial commit

---
+++ Page1.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace CurrencyApp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class Page1 : Page
+    {
+        public Page1()
+        {
+            this.InitializeComponent();
+        }
+
+        private void p1exit_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Exit();
+        }
+
+        private void USD_to_GBP_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(Page1));
+        }
+
+        private void GBP_to_USD_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(Page2));
+        }
+
+        private void GBP_to_Euro_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(Page3));
+        }
+
+        private void Euro_to_GBP_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(Page4));
+        }
+    }
+}
